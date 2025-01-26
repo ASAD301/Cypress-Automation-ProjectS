c1d908a368055a8239839012e04c266aebcf1b5b
ASAD301 <asad.info100@gmail.com>
2025-01-26 11:37:52 +0600

    CSV file read and asssertion

--- cypress/integration/examples/API/SessionTest.js
+++ cypress/integration/examples/API/SessionTest.js
@@ -1,8 +1,11 @@
 /// <reference types = "cypress"/>
 //const cypress = require("cypress")
+const neatCSV = require("neat-csv")
+let pname; // product name 
+let orderNumbr;
 
 describe("Api Session ",()=>{
-    it("loged in through local storaage",()=>{
+    it("loged in through local storaage",async ()=>{
         cy.LoginAPI().then(function()
         {
             cy.visit("https://rahulshettyacademy.com/client/",
@@ -12,5 +15,36 @@ describe("Api Session ",()=>{
                 }
             })
         })
+        cy.get(".card-body b").eq(1).then((element)=>{
+             pname = element.text();
+            cy.log(pname)
+        })
+        cy.get(".card-body ").eq(1).contains("Add To Cart").click();
+        cy.get("button[routerlink*='cart']").click();
+        cy.get("button").contains("Checkout").click();
+        cy.get(".user__name .user__address").type("Ban")
+        cy.get(".ta-results button").each(($el, index , $list)=>{
+            const country = $el.text();
+            cy.log(country);
+            if(country === " Bangladesh"){
+                cy.wrap($el).click();
+                return false; 
+            }
+        })
+        cy.get(".action__submit").click();
+        cy.wait(3000);
+        cy.get("tr button").contains("CSV").click();
+        
+        //Cypress.config("fileServerFolder")  to create download file global path
+        // cy.readfile > read file from path 
+        cy.readFile(Cypress.config("fileServerFolder")+ "/cypress/downloads/order-invoice_asad.jatri.csv")
+        .then(async (text)=>{
+            const csv = await neatCSV(text);
+            console.log(csv);
+            const purchaseName = csv[0]["Product Name"];
+            const purchseID = csv[0]["Invoice Number"]
+            expect(pname).to.equal(purchaseName);
+        })
+        
     })
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,13 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add("LoginAPI",()=>{
+    
+    // any api call cy.request()
+    cy.request("POST", "https://rahulshettyacademy.com/api/ecom/auth/login", 
+        {"userEmail": "asad.jatri@gmail.com", "userPassword": "$Learning1"}).then(function(respons){
+            expect(respons.status).to.equal(200);
+            Cypress.env("token", respons.body.token)
+        })   
+})
